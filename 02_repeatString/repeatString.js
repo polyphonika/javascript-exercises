94c1eb0b88247e27b00f078c03608fbfff31ea31
Ryan Berry <email@cornerhousemedia.com>
2024-08-11 20:36:47 +0100

    compelted lesson 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,18 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    let returnedString = '';
+    for (let i = 0; i < num; i++) {
+        // console.log(string)
+        returnedString += string;    
+    }
 
+    if (num < 0) {
+        return 'ERROR'
+    } else {
+        return returnedString
+    }
 };
+
+// repeatString('hey', 3)
 
 // Do not edit below this line
 module.exports = repeatString;
