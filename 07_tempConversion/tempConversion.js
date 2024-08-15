ab34f88f4ce0f2f705a3982b03b932152bd56c82
Ryan Berry <email@cornerhousemedia.com>
2024-08-15 17:17:29 +0100

    completed lesson 07 temperature conversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,25 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  let celcius = ((fahrenheit-32)*5)/9
+  return parseFloat(celcius.toFixed(1))
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celcius) {
+  let fahrenheit = (celcius*(9/5))+32
+  return parseFloat(fahrenheit.toFixed(1))
 };
+
+
+/*
+Calculations and rounding
+F to C: °C = [ (°F-32)×5]/9
+C to F: °F = (°C * 9/5) + 32
+
+JS rounding to one dp:
+toFixed(x) where x is number of dp
+but this outputs a string
+use parseFloat to convert back to float
+
+*/
 
 // Do not edit below this line
 module.exports = {
