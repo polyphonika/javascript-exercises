1e1c7112c0c435fb6abdb4f04d0c16053e401964
Ryan Berry <email@cornerhousemedia.com>
2024-08-15 17:05:41 +0100

    completed lesson 06 leap years

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,41 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    if (year % 4 == 0) {
+        
+        if (year % 100 == 0) {
+            if (year % 400 == 0)
+            {
+                return true
+            } else {
+                return false
+            }
+        }       
+        
+        return true
+        } else {
+        return false;
 
-};
+        }    
+}
+
+
+
+
+
+/*
+LOGIC CALCULATIONS
+if divisible by 4, leap year
+if divisible by 100, not leap year unless divisible by 400
+
+from solution: this is a great way of testing, i like it:
+
+const isYearDivisibleByFour = year % 4 === 0;
+  const isCentury = year % 100 === 0;
+  const isYearDivisibleByFourHundred = year % 400 === 0;
+
+then simply test truthy or not. brilliant
+
+
+*/
 
 // Do not edit below this line
 module.exports = leapYears;
