3b1c9c4b418b9f9c0ac70c871ff716b192ee8696
Ryan Berry <email@cornerhousemedia.com>
2024-08-15 13:47:53 +0100

    lesson 4 removeFromArray completed

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,21 @@
-const removeFromArray = function() {
+const removeFromArray = function(processedArray, ...argsToRemove) {
+
+    const newArray = [];
+    processedArray.forEach((element) => {
+        if(!argsToRemove.includes(element)){
+            newArray.push(element);
+        }
+
+        
+    });
+
+
+
+return newArray;
+
 };
+
+// console.log(removeFromArray([1,2,3,4],3));
 
 // Do not edit below this line
 module.exports = removeFromArray;
