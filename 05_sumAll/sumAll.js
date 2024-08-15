da4c43ed9f77c669875d0629e041de827d8ff9fb
Ryan Berry <email@cornerhousemedia.com>
2024-08-15 14:23:34 +0100

    lesson 05 sumAll completed

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
+const sumAll = function(a,b) {
+    
+    if (a<0 || b<0 || !Number.isInteger(a) || !Number.isInteger(b)) {
+        return 'ERROR';
+    }    
 
+    if (a>b) {
+        let c=a;
+        a=b;
+        b=c;
+    }
+    
+    let sumResult = 0;
+    for (let i=a;i<=b;i++) {
+        sumResult+=i;
+        }
+
+    return sumResult;
 };
 
 // Do not edit below this line
