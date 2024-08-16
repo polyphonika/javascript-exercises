2652a96b9964f947e81cc7679169b7d756acd1e0
Ryan Berry <email@cornerhousemedia.com>
2024-08-16 15:30:31 +0100

    completed lesson 08_calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,53 @@
-const add = function() {
-	
+const add = function(a,b) {
+	return a + b;  
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a-b;
 };
 
-const sum = function() {
-	
+const sum = function(numbers) {
+	let sum = 0;
+  if (numbers) {
+    numbers.forEach(number => {
+      sum += number;
+    });
+  }
+  return sum;
 };
 
-const multiply = function() {
+const multiply = function(numbers) {
 
+let arrayLength = numbers.length;
+let multiple = numbers[0];
+for (let i = 1; i <= arrayLength-1; i++ ) {
+  multiple *= numbers[i];
+}
+
+return multiple;
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	return a**b;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+	/*
+  what is a factorial?
+  eg factorial of 5 = 5 x 4 x 3 x 2 x 1
+  nb: factorial of 0 is 1
+  */
+
+  if (a==0 || a==1) {
+    return 1;
+  } else {
+    let result = a;
+    for (let i = a-1; i>=1; i--) {
+      result *= i;
+    }
+    return result;
+  } 
+
 };
 
 // Do not edit below this line
