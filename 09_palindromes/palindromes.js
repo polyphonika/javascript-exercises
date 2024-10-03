df25fad2e5161db9ea6d7538d9688c2fa34445f2
Ryan Berry <email@cornerhousemedia.com>
2024-10-03 14:58:29 +0100

    palindromes done

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,10 @@
-const palindromes = function () {
-
+const palindromes = function (item) {
+    const itemReversed = item.replace(/[^a-z0-9]/gi, '').toLowerCase().split('').reverse().join('');
+    if (itemReversed === item.replace(/[^a-z0-9]/gi, '').toLowerCase()) {
+        return true
+    } else {
+        return false
+    }
 };
 
 // Do not edit below this line
